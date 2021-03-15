64e094cbb7581b59d5dc97f0be62ff9c2a9bd8ed
unknown <jamesdparkny@gmail.com>
2021-03-15 13:01:19 -0400

    fix

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+const cors = require('cors')
 
+app.use(cors())
 
 let notes = [
   {
@@ -79,7 +81,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
